ec902a24613826ac27de6258474064a1668b775d
Adrian Torchiana <adrian.torchiana@gmail.com>
2021-10-07 17:38:23 -0700

    Train model, run prediction

--- src/fit.py
+++ src/fit.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from tensorflow.keras import callbacks
 
 from generator import get_generator
 from model import get_model
@@ -20,6 +21,26 @@ def main(image_shape=(64, 64, 1), n_classes=3, n_blocks=4, batch_size=20):
 
     plt.imshow(Y[0, :, :, :].argmax(axis=-1))
     plt.savefig("example_Y.png")
+    plt.close()
+
+    history = model.fit(
+        x=generator,
+        steps_per_epoch=50,
+        epochs=10,
+        verbose=True,
+        callbacks=[
+            callbacks.EarlyStopping(
+                patience=20, monitor="val_loss", restore_best_weights=True, verbose=True
+            )
+        ],
+        validation_data=generator,  # We use the same generator for training and validation
+        validation_steps=50,
+    )
+
+    predictions = model.predict(X)
+
+    plt.imshow(predictions[0, :, :, :].argmax(axis=-1))
+    plt.savefig("example_predictions.png")
     plt.close()
 
 
